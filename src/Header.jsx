90ca60d7e466b6db0f0d8eace9bdea5115186049
Dan-Victor <victoradesola8@gmail.com>
2025-05-21 17:01:13 +0100

    first version

---
+++ src/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+return (
+ <header className='w-full h-[80px] flex justify-between items-center px-[12px] sm:px-[20px] lg:px-[45px]'>
+    <h1 className='text-[#1599e6] text-[18px] sm:text-[30px] font-bold'>Trustesse Dashboard</h1>
+    <nav className='flex gap-[10px] sm:gap-[70px] text-[#000] text-[16px] sm:text-[20px] lg:pr-20'>
+        <a
+          href="/"
+          className=' hover:border-b-3 hover:border-b-[#1599e6] '
+        >
+          Home
+        </a>
+        <a href="#" className='hover:border-b-3 hover:border-b-[#1599e6]'>
+          Post Job
+        </a>
+    </nav>
+ </header>
+)
+}
+
+export default Header
